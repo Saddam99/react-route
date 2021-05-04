a5731c51bb089568b20d6a07d4c98c4155a228f2
Saddam <visinwork@gmail.com>
2021-05-04 18:54:11 +0300

    'map'

--- src/components/content/Contact.js
+++ src/components/content/Contact.js
@@ -16,7 +16,7 @@ const Contact = () => (
                 controls: []
             }}
             width="100%"
-            height="100%"
+            height="500px"
         >
             {images.map((n) => (
                 <Placemark
